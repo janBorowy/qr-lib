7d29cf512ff48ecc8f140afcb68747389f11ac74
Jan Borowy <01168874@pw.edu.pl>
2026-07-21 16:05:05 +0200

    add cli11 dependency to cli

--- cli/main.cpp
+++ cli/main.cpp
@@ -1,22 +1,8 @@
+#include "CLI/App.hpp"
 #include "qrlib.h"
 
-int main() {
-
-    // ver 8
-    // auto qr =
-    //     generate_qr("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-    //                 "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
-    //                 qr::ErrorCorrectionLevel::Q);
-
-    // ver 2
-    // auto qr = with_quiet_zone(generate_qr("Hello, world!", qr::Q));
-
-    // ver 1
-    // auto qr = generate_qr("Hello", qr::Q);
-    auto qr =
-        generate_qr("https://www.youtube.com/"
-                    "watch?v=gpxy71WGzI4&list=RDgpxy71WGzI4&start_radio=1",
-                    qr::L);
-    qr.display("Your qr code");
+int main(int argc, char **argv) {
+    CLI::App app{"qr-cli"};
+    argv = app.ensure_utf8(argv);
     return 0;
 }
